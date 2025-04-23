fdb5de7b8981ad200613c5ec928cb303e75e84d6
Mateo Muscolino <muscolinomateo@gmail.com>
2025-04-23 10:13:27 -0300

    ej2 TP2

--- TP2/script.js
+++ TP2/script.js
@@ -1,4 +1,4 @@
-//ej 1
+/*ej 1
 let titulo = document.getElementById("tituloPrincipal");
 titulo.textContent = "Titulo modificado"
 
@@ -9,4 +9,22 @@ for (let i = 0; i < parrafos.length; i++) {
 let todosLosLi = document.querySelectorAll("li"); // te devuelve una lista con todos los li del documento
 for (let i = 0; i<todosLosLi.length; i++){
     todosLosLi[i].textContent += " Texto agregado" //agregar texto al que tiene
-}
+}*/
+
+//ej2
+document.querySelector("button").addEventListener("click", function () {
+    let input = document.querySelector("input");// captura el valor del input
+    let inputValue = input.value;
+
+    if (inputValue.trim() !== "") {
+        let nuevoLi = document.createElement("li"); // crea un nuevo li con el valor del input // el trim saca los espacios
+        nuevoLi.textContent = inputValue;
+        let botonEliminar = document.createElement("button");// crea un botón "Eliminar"
+        botonEliminar.textContent = "Eliminar";
+        botonEliminar.addEventListener("click", function () {// agrega evento al botón "Eliminar"
+            nuevoLi.remove(); // elimina el li al hacer clic en el boton
+        });
+        nuevoLi.appendChild(botonEliminar); // agregar el boton al li
+        document.querySelector("ul").appendChild(nuevoLi);// agrega el li a la lista
+    }
+});
