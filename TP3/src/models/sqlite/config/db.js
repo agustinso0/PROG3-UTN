02a3a0ee1d6eeb03ec28ac6b2326b02a38119a7a
Agustin Loos <loosagustin@gmail.com>
2025-06-07 10:39:39 -0300

    Habilitar el registro de consultas en la configuración de Sequelize y ajustar la sincronización de la base de datos

--- TP3/src/models/sqlite/config/db.js
+++ TP3/src/models/sqlite/config/db.js
@@ -4,12 +4,16 @@ const path = require("path");
 const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: path.join(__dirname, "db", "clinica.sqlite"),
-  logging: false,
+  logging: true,
 });
+
+/* require("../../sqlite/entities/paciente.entity");
+require("../../sqlite/entities/turno.entity"); */
 
 const connectDB = async () => {
   try {
-    await sequelize.sync();
+    await sequelize.sync({ alter: true }); 
+    console.log(await sequelize.getQueryInterface().showAllTables());
     console.log("Base de datos conectada.");
   } catch (error) {
     console.error("Error conectando a la base de datos:", error);
