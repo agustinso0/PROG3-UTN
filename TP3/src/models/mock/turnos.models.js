4d7d971c387f207a6f8d3bc1830e3e558b2a6beb
Agustin Loos <loosagustin@gmail.com>
2025-06-07 10:39:30 -0300

    Refactor TurnosModel: reemplazar manejo de datos en memoria por integración con modelo Sequelize

--- TP3/src/models/mock/turnos.models.js
+++ TP3/src/models/mock/turnos.models.js
@@ -1,31 +1,22 @@
-const turnos = require("../../../data/turnos.json");
+const turnos = require("../sqlite/turno.model.js");
 
 class TurnosModel {
   static async obtenerTodosLosTurnos() {
-    return turnos;
+    return await turnos.getTurnosModel();
   }
 
   static async listarPorPaciente(idPaciente) {
-    const turnoPaciente = turnos.filter(
-      (t) => String(t.idPaciente) === String(idPaciente)
-    );
-    return turnoPaciente;
+    return await turnos.getTurnosByPacienteModel(idPaciente);
   }
 
   static async delete(idTurno) {
-    let eliminado = false;
-    const index = turnos.findIndex((t) => String(t.id) === String(idTurno));
-    if (index === -1) {
-      turnos.splice(index, 1);
-      eliminado = true;
-    }
-    return eliminado;
+    const eliminado = await turnos.deleteTurnoModel(idTurno);
+    // Sequelize retorna el número de filas eliminadas
+    return eliminado > 0;
   }
 
   static async crearTurno(turno) {
-    turno.id = turnos.length ? turnos[turnos.length - 1].id + 1 : 1;
-    turnos.push(turno);
-    return turno;
+    return await turnos.createTurnoModel(turno);
   }
 }
 
