819fa2dd7f95dcbfc18041422df941800e80bc82
Mateo Muscolino <muscolinomateo@gmail.com>
2025-06-07 09:52:20 -0300

    Validacion de los id ingresados por los usuraios

--- TP3/practico3progra3/src/routes/turnos.routes.js
+++ TP3/practico3progra3/src/routes/turnos.routes.js
@@ -3,10 +3,10 @@
 const express = require('express');
 const router = express.Router();
 const TurnosController = require('../controllers/API/turnos.controller');
+const {validarParametroID} = require('../middleware/validate')
 
-
-router.get('/:idPaciente', TurnosController.obtenerPorID);
+router.get('/:idPaciente',validarParametroID('idPaciente'),TurnosController.obtenerPorID);
 router.get('/', TurnosController.obtenerTurnos);
-router.delete('/:idTurno', TurnosController.delete);
+router.delete('/:idTurno',validarParametroID('idTurno') ,TurnosController.delete);
 
 module.exports = router;
