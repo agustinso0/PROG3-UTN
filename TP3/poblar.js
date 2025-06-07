3812c2da4b0637bbf8b7d50f32d636f2e7631f20
Agustin Loos <loosagustin@gmail.com>
2025-06-07 10:43:29 -0300

    Agregar función para poblar la base de datos con pacientes y turnos

--- TP3/poblar.js
+++ TP3/poblar.js
@@ -1,4 +1,5 @@
 const { Paciente } = require("./src/models/sqlite/entities/paciente.entity.js");
+const { Turno } = require("./src/models/sqlite/entities/turno.entity.js");
 
 async function poblarPacientes() {
   await Paciente.bulkCreate(
@@ -15,6 +16,43 @@ async function poblarPacientes() {
     ],
     { ignoreDuplicates: true }
   );
+
+  await Turno.bulkCreate(
+    [
+      {
+        id: 1,
+        id_paciente: 1,
+        date: new Date("2023-10-01T10:00:00Z"),
+        motivo: "Consulta general",
+      },
+      {
+        id: 2,
+        id_paciente: 2,
+        date: new Date("2023-10-02T11:00:00Z"),
+        motivo: "Chequeo anual",
+      },
+      {
+        id: 3,
+        id_paciente: 3,
+        date: new Date("2023-10-03T12:00:00Z"),
+        motivo: "Control de salud",
+      },
+      {
+        id: 4,
+        id_paciente: 4,
+        date: new Date("2023-10-04T13:00:00Z"),
+        motivo: "Consulta médica",
+      },
+      {
+        id: 5,
+        id_paciente: 5,
+        date: new Date("2023-10-05T14:00:00Z"),
+        motivo: "Revisión de salud",
+      },
+    ],
+    { ignoreDuplicates: true }
+  );
+
   console.log("Pacientes cargados");
 }
 
