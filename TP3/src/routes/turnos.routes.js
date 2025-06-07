69c191c00a685577da88746e4335cc2f5f65ea27
Agustin Loos <loosagustin@gmail.com>
2025-06-07 10:39:53 -0300

    Eliminar validaciones de parámetros en las rutas de turnos

--- TP3/src/routes/turnos.routes.js
+++ TP3/src/routes/turnos.routes.js
@@ -1,20 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const TurnosController = require("../controllers/API/turnos.controller");
-const { validarParametroID } = require("../middlewares/validate");
 
 router.get("/ver", TurnosController.renderizarTurnos);
-router.get(
-  "/:idPaciente",
-  validarParametroID("idPaciente"),
-  TurnosController.obtenerPorID
-);
+router.get("/:idPaciente", TurnosController.obtenerPorID);
 router.get("/", TurnosController.obtenerTurnos);
 router.post("/", TurnosController.crearTurno);
-router.delete(
-  "/:idTurno",
-  validarParametroID("idTurno"),
-  TurnosController.delete
-);
+router.delete("/:idTurno", TurnosController.delete);
 
 module.exports = router;
