666fdbf14eaae7ef8337ff3bcb4bf4ddff1e61d6
Agustin Loos <loosagustin@gmail.com>
2025-06-07 10:39:12 -0300

    Reorganizar importaciones en index.js para mejorar la estructura del código

--- TP3/src/index.js
+++ TP3/src/index.js
@@ -1,7 +1,8 @@
-const Server = require("./server.js");
+const { Paciente } = require("./models/sqlite/entities/paciente.entity.js");
+const { Turno } = require("./models/sqlite/entities/turno.entity.js");
 const { connectDB } = require("./models/sqlite/config/db.js");
+const Server = require("./server.js");
 
 connectDB();
 const server = new Server("ejs");
-
 server.listen();
