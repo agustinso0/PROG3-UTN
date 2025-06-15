0392bcca40f1167abb6fc2bbd7d3c821a31e5577
Mateo Muscolino <muscolinomateo@gmail.com>
2025-06-15 17:58:39 -0300

    Mejoras css

--- TP4/Backend/models/personaModelo.js
+++ TP4/Backend/models/personaModelo.js
@@ -1,9 +1,13 @@
 const personas = [
-  { id: 1, nombre: "Juan", apellido: "Pérez", edad: 25, email: "juan@example.com" },
+  { id: 1, nombre: "Mateo", apellido: "Muscolino", edad: 21, email: "mateo@gmail.com" },
   { id: 2, nombre: "Ana", apellido: "López", edad: 30, email: "ana@example.com" },
   { id: 3, nombre: "Luis", apellido: "Gómez", edad: 22, email: "luis@example.com" },
   { id: 4, nombre: "Marta", apellido: "Fernández", edad: 28, email: "marta@example.com" },
-  { id: 5, nombre: "Carlos", apellido: "Rodríguez", edad: 35, email: "carlos@example.com" }
+  { id: 5, nombre: "Carlos", apellido: "Rodríguez", edad: 35, email: "carlos@example.com" },
+  { id: 6, nombre: "Mateo", apellido: "Muscolino", edad: 21, email: "mateo@gmail.com" },
+  { id: 7, nombre: "Mateo", apellido: "Muscolino", edad: 21, email: "mateo@gmail.com" },
+  { id: 8, nombre: "Mateo", apellido: "Muscolino", edad: 21, email: "mateo@gmail.com" },
+
 ];
 
 export default personas;
--- TP4/Frontend/src/componentes/TraerPersonas.jsx
+++ TP4/Frontend/src/componentes/TraerPersonas.jsx
@@ -23,7 +23,7 @@ const TraerPersonas = () => {
 
     return (
         <div>
-            <h1>Lista de Personas</h1>
+            <h1>Lista de Personas TP4</h1>
             <ListarTarjetas personas = {personas}/>
         </div>
     )
